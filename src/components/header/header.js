2044bc89c512f19e789bb01e754986990ae28dc7
Marian Lyzhychka <52856608+mariankomen@users.noreply.github.com>
2021-02-18 14:30:02 +0200

    finally

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -4,27 +4,29 @@ import {NavLink} from "react-router-dom";
 import './header.css'
 
 const Header = () => {
-    return(
+    return (
 
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
-                        data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
-                        aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <a className="navbar-brand" href="#">Lyzhychka_LAB_1</a>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                    data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <a className="navbar-brand" href="#">Lyzhychka_LAB_1</a>
 
-                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul className="navbar-nav mr-auto mt-2 mt-md-0 links">
-                        <li className="nav-item active">
-                            <NavLink to='/area'>Обчислити площу</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink to='/perymetr'>Обчислити периметр</NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+            <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav mr-auto mt-2 mt-md-0 links">
+                    <li className="nav-item active">
+                        <NavLink to='/area'
+                                 activeClassName='active'>Обчислити площу</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to='/perymetr'
+                                 activeClassName='active'>Обчислити периметр</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
--- src/components/perimetr/perimetr.js
+++ src/components/perimetr/perimetr.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Perimetr = () => {
-    return(
-        <div>PERIMETER PAGE</div>
+    const [first, setFirst] = useState(0)
+    const [second, setSecond] = useState(0)
+    const [square, setSquare] = useState(0)
+
+    return (
+        <div>
+            <h1>Perymetr PAGE</h1>
+            <br/>
+
+            <input type='text'
+                   placeholder='Введіть сторону а:'
+                   onChange={(e) => setFirst(e.target.value)}
+            />
+
+            <input type='text'
+                   placeholder='Введіть сторону b:'
+                   onChange={(e) => setSecond(e.target.value) }
+            />
+            <button type="button"
+                    className="btn btn-success"
+                    onClick={() => setSquare((first*2)+(second*2))}
+            >Обчислити
+            </button>
+            <p>Периметр прямокутника: {square}</p>
+        </div>
+
     )
 }
 
